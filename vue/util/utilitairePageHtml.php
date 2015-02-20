44730ded3d8aed0f3efbc231fc1c1df37781da7a
afrenchie <quentin.malinge@gmail.com>
2015-02-20 09:53:50 +0100

    Update utilitairePageHtml.php

--- vue/util/utilitairePageHtml.php
+++ vue/util/utilitairePageHtml.php
@@ -35,6 +35,11 @@ public function genereEnteteHtml(){
 	$entete.= '<head>';
 	$entete.= '<meta charset="utf-8">';
 	$entete.= '<link href="vue/css/style.css" type="text/css" rel="stylesheet" />';
+	$entete.= '<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">';
+	$entete.='<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">';
+	$entete.='<script src="http://code.jquery.com/jquery.min.js"></script>';
+	$entete.='<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>';
+	$entete.='<script src="vue/js/javascript.js"></script>';
 	$entete.= '</head>';	
 	return $entete;
 }
