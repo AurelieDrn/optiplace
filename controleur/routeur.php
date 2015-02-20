6b0ebde20980b996bc9edff05cc23d41a7ce9c09
afrenchie <quentin.malinge@gmail.com>
2015-02-20 09:46:41 +0100

    Update routeur.php

--- controleur/routeur.php
+++ controleur/routeur.php
@@ -1,15 +1,18 @@
 <?php
+require_once "controleurMenuAccueil.php";
 require_once "controleurGererEtudiants.php";
 require_once "controleurGererSalles.php";
 require_once "controleurGererPlacement.php";
 require_once __DIR__."/../modele/dao/Dao.php";
 
 class Routeur {
+	private $ctrlMenuAccueil;
 	private $ctrlGererSalles;
 	private $ctrlGererEtudiants;
 	private $ctrlGererPlacement;
 
 	public function __construct(){
+		$this->ctrlMenuAccueil= new ControleurMenuAccueil();
 		$this->ctrlGererSalles= new ControleurGererSalles();
 		$this->ctrlGererEtudiants= new ControleurGererEtudiants();
 		$this->ctrlGererPlacement= new ControleurGererPlacement();
@@ -17,7 +20,10 @@ class Routeur {
 
 	//Traite une requête entrante
 	public function routerRequete() {
-		if(isset($_GET["placerPage"])){
+		if(isset($_GET["menu"])){
+			$this->ctrlMenuAccueil->accueilMenu();
+		}
+		else if(isset($_GET["placerPage"])){
 			$this->ctrlGererPlacement->accueilPla();
 		}
 		else if(isset($_GET["etuPage"])){
@@ -26,11 +32,8 @@ class Routeur {
 		else if(isset($_GET["sallesPage"])){
 			$this->ctrlGererSalles->accueilSal();
 		}
-		else if(isset($_GET["uploadSal"])) {
-			$this->ctrlGererSalles->uploadSal();
-		}
 		else {
-			$this->ctrlGererPlacement->accueilPla();
+			$this->ctrlMenuAccueil->accueilMenu();
 		}	
 	}
 }
